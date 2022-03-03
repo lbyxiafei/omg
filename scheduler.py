3db0ad22bd491c0788d09bc3347816885328e2b6
Bonfire <lbyxiafei@gmail.com>
2022-03-03 13:30:28 -0800

    Check point, 2022-03-03T13:30:28.

--- scheduler.py
+++ scheduler.py
@@ -1,23 +1,18 @@
 import sqlite3
 from logger import Logger
+from servant import Servant
 
 class Scheduler():
     def __init__(self):
         self.logger = Logger()
+        self.servant = Servant()
     
     def download(self, match_id):
         pass
 
-    def get_key(self):
-        con = sqlite3.connect(r"D:\\OMG\\OMG.db")
-        cur = con.cursor()
-        cur.execute("SELECT * FROM Coffer")
-        key = cur.fetchone()[1]
-        con.close()
-        return key 
-
-
 if __name__ == '__main__':
     scheduler = Scheduler() 
-    scheduler.logger.info("hello world")
-    print(scheduler.get_key())
+    key = scheduler.servant.get_key()
+    print(key)
+    keys = scheduler.servant.get_keys()
+    print(keys)
--- servant.py
+++ servant.py
@@ -1,13 +1,22 @@
+import sqlite3
+from logger import Logger
 
 class Servant():
     def __init__(self):
-        pass
+        self.logger = Logger()
 
     def get():
         pass
 
     def get_key(self):
-        pass
+        return self.get_keys()[0]
 
     def get_keys(self):
-        pass
+        con = sqlite3.connect(r"D:\\OMG\\OMG.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Coffer")
+        keys = []
+        for row in cur.fetchall():
+            keys.append(row[1])
+        con.close()
+        return keys 
