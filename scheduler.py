3d736d89603a968a53bb7bae77af556e506088a9
Bonfire <lbyxiafei@gmail.com>
2022-03-04 00:45:01 -0800

    Check point, 2022-03-04T00:45:01.

--- scheduler.py
+++ scheduler.py
@@ -7,12 +7,17 @@ class Scheduler():
         self.logger = Logger()
         self.servant = Servant()
     
-    def download(self, match_id):
-        pass
+    def test(self):
+        matches = self.servant.get_history()
+        for row in matches:
+            pass
+
+        min_id, max_id = min(x['match_id'] for x in matches), max(x['match_id'] for x in matches)
+        print(min_id, max_id, max_id-min_id)
+
+        match = self.servant.get_match(5425617019)
+
 
 if __name__ == '__main__':
     scheduler = Scheduler() 
-    key = scheduler.servant.get_key()
-    print(key)
-    keys = scheduler.servant.get_keys()
-    print(keys)
+    scheduler.test()
--- servant.py
+++ servant.py
@@ -1,22 +1,41 @@
+import requests
 import sqlite3
 from logger import Logger
 
 class Servant():
     def __init__(self):
         self.logger = Logger()
+        self.keys = []
+        self.get_keys()
 
-    def get():
+    def get_match(self, match_id):
+        url = f"https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v001/?match_id={match_id}&key={self.keys[0]}"
+        response = requests.get(url)
+        data = response.json()
+        return data['result']
+
+    def get_history(self):
+        url = f"https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key={self.keys[0]}"
+        response = requests.get(url)
+        data = response.json()
+        return data['result']['matches']
+    
+    def query_pool(self, min_id, max_id):
         pass
 
     def get_key(self):
+        if len(self.keys)>0:
+            return self.keys[0]
         return self.get_keys()[0]
 
     def get_keys(self):
+        if len(self.keys)>0:
+            return self.keys
         con = sqlite3.connect(r"D:\\OMG\\OMG.db")
         cur = con.cursor()
         cur.execute("SELECT * FROM Coffer")
-        keys = []
         for row in cur.fetchall():
-            keys.append(row[1])
+            self.keys.append(row[1])
         con.close()
-        return keys 
+        self.logger.info(f"Retrieve keys: {self.keys} from omg.logger.")
+        return self.keys
